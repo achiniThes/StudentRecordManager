8213bda5a32065ae5083f0b6fcd727da323a1262
achiniThes <achini.thesanya@gmail.com>
2026-05-04 16:13:09 +0530

    Implemented viewStudents feature

--- Main.java
+++ Main.java
@@ -5,11 +5,20 @@ public class Main {
         System.out.println("Student Record Manager");
         addStudent("Alice");
         addStudent("Bob");
+        viewStudents();
     }
 
     //Add students
     static void addStudent(String name) {
         students.add(name);
         System.out.println(name + " added.");
+    }
+
+    //View Students
+    static void viewStudents() {
+        System.out.println("Student List:");
+        for (String s : students) {
+            System.out.println(s);
+        }
     }
 }
